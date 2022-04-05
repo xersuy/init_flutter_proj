60b80a5ac47edd3e967147eada6fc6d06f8e188f
xersuy <dev.luyoes20@gmail.com>
2022-04-05 22:50:03 +0900

    Update README.md and main.dart

--- README.md
+++ README.md
@@ -56,3 +56,34 @@ margin: const EdgeInsets.all(8.0),
 ---
 
 ListBody(children: item.map((i) => Text('$i')).toList()),
+
+
+ final item = List.generate(100, (i) => i).toList();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page xet'),
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.event),
+              title: const Text('event'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.camera),
+              title: const Text('camera'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () {},
+            )
+          ],
+        ));
+        
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo xersuy',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -15,36 +15,95 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0;
+
+  final _pages = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    final item = List.generate(100, (i) => i).toList();
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo Home Page xet'),
-        ),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              trailing: const Icon(Icons.navigate_next),
-              onTap: () {},
+      appBar: AppBar(
+          title:
+              const Text('Xersuy Demo', style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                print('menu button is clicked');
+              },
+              icon: const Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+
+              // onPressed: () {}, //Icon(Icons.search, color: Colors.black),
             ),
-            ListTile(
-              leading: const Icon(Icons.event),
-              title: const Text('event'),
-              trailing: const Icon(Icons.navigate_next),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.camera),
-              title: const Text('camera'),
-              trailing: const Icon(Icons.navigate_next),
-              onTap: () {},
-            )
-          ],
-        ));
+          ]),
+      body: _pages[_index],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        currentIndex: _index,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Settings'),
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[_buildTop(), _buildMiddle(), _buildBottom()],
+    );
+  }
+
+  Widget _buildTop() {
+    return const Text('top');
+  }
+
+  Widget _buildMiddle() {
+    return const Text('middle');
+  }
+
+  Widget _buildBottom() {
+    return const Text('bottom');
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('이용서비스', style: TextStyle(fontSize: 40)),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('내정보', style: TextStyle(fontSize: 40)),
+    );
   }
 }
